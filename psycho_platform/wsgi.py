d6468938c1d57740e9d52a890e8987858de6a2a5
MetalFKingRawner <150486340+MetalFKingRawner@users.noreply.github.com>
2025-06-04 21:35:00 -0600

    Update wsgi.py

--- psycho_platform/wsgi.py
+++ psycho_platform/wsgi.py
@@ -12,5 +12,9 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'psycho_platform.settings')
+# Añade esto antes de get_wsgi_application()
+from whitenoise import WhiteNoise
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=settings.STATIC_ROOT)  # ✅ AÑADE
+
