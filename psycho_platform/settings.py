9547630cd88f68229180ed22f85b4c1e86d2252e
MetalFKingRawner <150486340+MetalFKingRawner@users.noreply.github.com>
2025-06-04 18:55:17 -0600

    Update settings.py

--- psycho_platform/settings.py
+++ psycho_platform/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # ← Lo añadimos aquí
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,9 +136,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'psycho_platform/static/'
+#STATIC_URL = 'psycho_platform/static/'
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
+#STATICFILES_DIRS = [BASE_DIR / 'psycho_platform/static']
+
+# Archivos estáticos (CSS, JS, imágenes)
+STATIC_URL = '/static/'
+
+# Carpeta donde collectstatic volcará todos los archivos estáticos en producción
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'psycho_platform/static']
+
+# Carpeta(s) con tus archivos estáticos en desarrollo
+STATICFILES_DIRS = [BASE_DIR / 'psycho_platform' / 'static']
+
+# WhiteNoise: agrega compresión y cache busting
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
