d0dcde68a8333e09098949afa2167a23e23fe4aa
MetalFKingRawner <150486340+MetalFKingRawner@users.noreply.github.com>
2025-06-16 15:19:53 -0600

    Actualización de urls
    Se agregaron las autenticaciónes del login

--- tests/urls.py
+++ tests/urls.py
@@ -1,19 +1,22 @@
 from django.urls import path
 from . import views
+from core.views import prueba_requiere_login
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('valores/', views.inicio_test_valores, name='inicio_test_valores'),
-    path('valores/segunda', views.inicio_segundo_test_valores, name='inicio_segundo_test_valores'),
+    path('valores/', login_required(views.inicio_test_valores), name='inicio_test_valores'),
+    path('valores/segunda', login_required(views.inicio_segundo_test_valores), name='inicio_segundo_test_valores'),
     path('valores/primera-parte/', views.primera_parte_valores, name='primera_parte_valores'),  # ← USADO EN FORM
     path('valores/segunda-parte/', views.segunda_parte_valores, name='segunda_parte_valores'),  # ← USADO EN FORM
     path('calcular-resultados-valores/', views.calcular_resultados_valores, name='calcular_resultados_valores'),
     path('resultados-valores/', views.mostrar_resultados_valores, name='mostrar_resultados_valores'),
     path('reporte-valores/<int:resultado_id>/', views.generar_pdf_valores, name='reporte_valores'),
     # URLs para el Test de Dominó
-    path('domino/', views.inicio_domino, name='inicio_domino'),
+    path('domino/', login_required(views.inicio_domino), name='inicio_domino'),
     path('domino/test/', views.test_domino, name='test_domino'),
     path('domino/ejercicio/<int:problema_id>/', views.detalle_problema_domino, name='detalle_problema_domino'),
     path('test-simple/', views.test_view, name='test_simple'),
     path('domino/calcular-resultados/', views.calcular_resultados_domino, name='calcular_resultados_domino'),
     path('domino/resultados/<int:resultado_id>/', views.resultados_domino, name='resultados_domino'),
+    path('requiere-login/', prueba_requiere_login, name='prueba_requiere_login'),
 ]
